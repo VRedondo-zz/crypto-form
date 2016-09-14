afd3cae7e8a949712132b4f22f79d1372c46ba65
Victor Redondo <victor.redondo@prodigious.cr>
2016-09-14 14:48:44 -0600

    Add cryptico-js and jsencrypt

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-var CryptoJS = require("crypto-js");
-var $ = require("jquery");
+// var CryptoJS = require("crypto-js");
+// var jsencrypt = require('jsencrypt'),
+var cryptico = require('cryptico-js'),
+    $ = require("jquery");
 
 $(function(){
     var formData = '',
-        domain = 'http://www.google.com?data=';
+        domain = 'http://www.google.com?data=',
+        publicKey = '03Ex5xFSxWhVmWozRB7OiFm2AwiO4oly1wuUkaB5n8cWiY/GD8W4r24m2sM0TEsfqo8y0QSS0uFz5zOCx3EpLjqVmkrcux5R8UtTzhfaP9QsQBtlP+6V79W40HHzy/QnYps5oNK6lfVxAmq5Vraztl5XzBPjpeinDOyU7dJGRhc=';
     $('input').on('blur, change', function(){
         formData = $('#userName').val() + '/' + $('#userLastName').val() + '/' + $('#userEmail').val();
-        var encriptUrl = domain+CryptoJS.AES.encrypt(formData, "public key");
+        // CriptoJS
+        // var encriptUrl = domain+CryptoJS.AES.encrypt(formData, publicKey);
+
+        // JSEncrypt
+        // var encrypt = new jsencrypt.JSEncrypt();
+        // encrypt.setPublicKey(publicKey);
+        // var encriptUrl = domain+encrypt.encrypt(formData);
+
+        // Cryptico
+        var encriptUrl = domain + cryptico.encrypt(formData, publicKey).cipher;
         $('#submitBtn').prop('href',encriptUrl);
         $('#resultUrl').val(encriptUrl);
     });
