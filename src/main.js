c9a94ad1e0ea9fafce856665f76bf1d976fb08de
Victor Redondo <victor.redondo@prodigious.cr>
2016-09-19 08:37:20 -0600

    Remove JS cryptico crypto and jsencrypt libraries Add .net Crypt libraries Add end point and encode crypted url

--- src/main.js
+++ src/main.js
@@ -1,25 +1,53 @@
-// var CryptoJS = require("crypto-js");
-// var jsencrypt = require('jsencrypt'),
-var cryptico = require('cryptico-js'),
-    $ = require("jquery");
+var $ = require("jquery");
+
+var xmlParamsDefault = "<RSAKeyValue><Modulus>xlIAW9ORaTQp7kapjSjQ6NyXYo11UdrHP+m2uXJTZotMomf5cpVgXjTuldt5JZGU+uhRkMsxECbdPnAFXikA/sLB66B5GtW2g/FXT4VmcvCAwgQalUOkX/WxPibiSnwzCONMVkd2WFf3HjPIL4dwLB19md4HP8pKqHa6CZjStg0=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+
+function GetNewRsaProvider(dwKeySize) {
+    // Create a new instance of RSACryptoServiceProvider.
+    if (!dwKeySize) dwKeySize = 1024;
+    return new System.Security.Cryptography.RSACryptoServiceProvider(dwKeySize);
+}
+
+function GetRsaKey() {
+    // ------------------------------------------------
+    // RSA Keys
+    // ------------------------------------------------
+    var rsa = GetNewRsaProvider();
+    // Import parameters from xml.
+    rsa.FromXmlString(xmlParamsDefault);
+    // Export RSA key to RSAParameters and include:
+    //    false - Only public key required for encryption.
+    //    true  - Private key required for decryption.
+    return rsa.ExportParameters(false);
+}
+
+function Encrypt(data) {
+    console.log('About to crypt '+data);
+    var decryptedBytes = System.Text.Encoding.UTF8.GetBytes(data);
+    var doOaepPadding = false;
+    // ------------------------------------------------
+    // Encrypt
+    // ------------------------------------------------
+    var rsa = GetNewRsaProvider();
+    // Import the RSA Key information.
+    rsa.ImportParameters(GetRsaKey(false));
+    // Encrypt the passed byte array and specify OAEP padding.
+    var encryptedBytes = rsa.Encrypt(decryptedBytes, doOaepPadding);
+    var encryptedString = System.Convert.ToBase64String(encryptedBytes)
+    // ------------------------------------------------
+    // Display the encrypted data.
+    return encryptedString;
+}
 
 $(function(){
     var formData = '',
-        domain = 'http://www.google.com?data=',
-        publicKey = '03Ex5xFSxWhVmWozRB7OiFm2AwiO4oly1wuUkaB5n8cWiY/GD8W4r24m2sM0TEsfqo8y0QSS0uFz5zOCx3EpLjqVmkrcux5R8UtTzhfaP9QsQBtlP+6V79W40HHzy/QnYps5oNK6lfVxAmq5Vraztl5XzBPjpeinDOyU7dJGRhc=';
+        encriptUrl = '',
+        domain = 'https://abbvie44.digitashealth.com/RSAListener?refinfo=';
+
     $('input').on('blur, change', function(){
-        formData = $('#userName').val() + '/' + $('#userLastName').val() + '/' + $('#userEmail').val();
-        // CriptoJS
-        // var encriptUrl = domain+CryptoJS.AES.encrypt(formData, publicKey);
-
-        // JSEncrypt
-        // var encrypt = new jsencrypt.JSEncrypt();
-        // encrypt.setPublicKey(publicKey);
-        // var encriptUrl = domain+encrypt.encrypt(formData);
-
-        // Cryptico
-        var encriptUrl = domain + cryptico.encrypt(formData, publicKey).cipher;
-        $('#submitBtn').prop('href',encriptUrl);
-        $('#resultUrl').val(encriptUrl);
+        formData = $('#userName').val() + '|' + $('#userLastName').val() + '|' + $('#userEmail').val();
+        encriptUrl = encodeURIComponent(Encrypt(formData));
+        $('#submitBtn').prop('href',domain+encriptUrl);
+        $('#resultUrl').val(domain+encriptUrl);
     });
 });
