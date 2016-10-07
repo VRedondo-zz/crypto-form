2c06c64eb4af15782245b9f8a7d14995280df2fa
Victor Redondo <victor.redondo@prodigious.cr>
2016-10-07 10:34:23 -0600

    Remove refInfo param,

--- js/EncryptJS.js
+++ js/EncryptJS.js
@@ -3,7 +3,6 @@ var EncryptJS = EncryptJS || {};
 EncryptJS.getUrl = (function(){
     function EncryptJS(){
         this.RSAKey = "<RSAKeyValue><Modulus>xlIAW9ORaTQp7kapjSjQ6NyXYo11UdrHP+m2uXJTZotMomf5cpVgXjTuldt5JZGU+uhRkMsxECbdPnAFXikA/sLB66B5GtW2g/FXT4VmcvCAwgQalUOkX/WxPibiSnwzCONMVkd2WFf3HjPIL4dwLB19md4HP8pKqHa6CZjStg0=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
-        this.endpoint = 'refinfo=';
     }
 
     EncryptJS.prototype.GetNewRsaProvider = function(dwKeySize) {
@@ -48,7 +47,7 @@ EncryptJS.getUrl = (function(){
             dataToEncrypt = ' ';
         }
         // return encoded URL
-        return self.endpoint+encodeURIComponent(self.Encrypt(dataToEncrypt));
+        return encodeURIComponent(self.Encrypt(dataToEncrypt));
     }
 
     var self = new EncryptJS();
